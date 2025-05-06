adfbfed2fbfe8378a0b119e3c5f12bc5851d27f8
Arias Edson <edson.arias@tecsup.edu.pe>
2025-05-06 11:47:59 -0500

    prueba

---
+++ LAB08-AriasApazaEdsonAnthony/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LAB08_AriasApazaEdsonAnthony.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
